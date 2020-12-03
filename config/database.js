152b0760d23fe56c3067d6919a6fd59a025dd9d1
Ty <ltgray579@gmail.com>
2020-12-03 14:00:04 -0700

    first heroku deployment

--- config/database.js
+++ config/database.js
@@ -14,4 +14,9 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize'
   },
+  production: {
+    use_env_variable: 'postgres://laejledsdnyxad:05e124d073402a3c40a0f883054e81bdb6e5223a028f47c22beeb17ad124c3b7@ec2-54-163-47-62.compute-1.amazonaws.com:5432/dbmrom5ft75b8v',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
