0dac78a946e1d7acfc12b978119507b2b20960ec
Ty <ltgray579@gmail.com>
2020-12-03 14:07:28 -0700

    heroku round 2

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ module.exports = {
     seederStorage: 'sequelize'
   },
   production: {
-    use_env_variable: 'postgres://laejledsdnyxad:05e124d073402a3c40a0f883054e81bdb6e5223a028f47c22beeb17ad124c3b7@ec2-54-163-47-62.compute-1.amazonaws.com:5432/dbmrom5ft75b8v',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
