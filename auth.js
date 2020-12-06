83c1d6852d9503fdfc6c6f384c0e08bc6ab2faae
Derek Nungesser <nungesserfilms@gmail.com>
2020-12-06 15:08:00 -0500

    routing for logout button is done

--- auth.js
+++ auth.js
@@ -1,9 +1,16 @@
+const db = require('./db/models');
+
 const loginUser = (req, res, user) => {
   req.session.auth = {
     username: user.id,
   };
 };
 
+const logoutUser = (req, res) => {
+  delete req.session.auth;
+};
+
 module.exports = {
   loginUser,
+  logoutUser
 };
