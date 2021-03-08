c289ddf3549badea237462b920992fd694f49cb5
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-03-08 11:10:40 -0500

    change review seed - input actual fake reviews

--- db/seeders/20201202164541-reviews.js
+++ db/seeders/20201202164541-reviews.js
@@ -1,32 +1,38 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Reviews', [{
-          body: 'Insert review test here',
+    return queryInterface.bulkInsert(
+      "Reviews",
+      [
+        {
+          body: "My favorite joke about the color red!",
           userId: 1,
           laughId: 1,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
         {
-          body: 'It would have been fun to have quirky and funny reviews in our seeder but I\'m not much of a comic',
+          body:
+            "It would be fun to have quirky and funny reviews in our seeder but I'm not much of a comic",
           userId: 2,
           laughId: 2,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
         {
-          body: 'Additional review text',
+          body: "I'm not a doctor, and I love this joke",
           userId: 2,
           laughId: 1,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
-      ], {});
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-     return queryInterface.bulkDelete('Review', null, {});
-  }
+    return queryInterface.bulkDelete("Review", null, {});
+  },
 };
