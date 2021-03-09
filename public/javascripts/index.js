1749076643117f0adff8df299481be0edef662fb
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-03-08 20:03:17 -0500

    working on: firing off bow REST from click. In index.js in the javascripts file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,22 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+window.addEventListener("load", (event) => {
+  console.log("hello from javascript!");
+});
+
+const bows = document.getElementsByClassName("bow");
+for (let i = 0; i < bows.length; i++) {
+  bows[i].addEventListener("click", (event) => {
+    console.log(event.target.attributes);
+    // fire off window.location.href, flip whatever is in the database
+  });
+}
+
+// window.addEventListener("onClick", (event) => {
+//   console.log(event.target.id);
+//   //       script(src="/javascripts/index.js" type="module" defer)
+//   //   -
+//   //     function rateBows() {
+//   //       console.log("I am here.");
+//   //       //- window.location.href = "/laughs/" + laugh.id + "/ratings/";
+//   //     }
+//   //     rateBows()
+// });
