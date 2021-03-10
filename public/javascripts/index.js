977310da56ddc31335e983aafbe286bd364b1add
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-03-10 18:10:47 -0500

    get laughboxid everytime laughbox dropdown changes and hit the laughboxes route

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -19,16 +19,13 @@ for (let i = 0; i < bows.length; i++) {
 const laughboxes = document.getElementsByClassName("laughboxes");
 for (let i = 0; i < laughboxes.length; i++) {
   laughboxes[i].addEventListener("change", (event) => {
-    // const laughContainer = event.currentTarget.parentNode;
-    // const childNodes = laughContainer.childNodes;
-    // let laughId;
-    // for (let node of childNodes) {
-    //   if (node.id === "laughId") {
-    //     laughId = node.attributes.name.nodeValue;
-    //   }
-    // }
-    console.dir(event)
     let laughBoxId;
+
+    const selectedIndex = event.target.options.selectedIndex;
+
+    if (selectedIndex !== 0) {
+      laughBoxId = event.target[selectedIndex].attributes[0].nodeValue;
+    }
     if (laughBoxId !== undefined) {
       location.href = "laughboxes/" + laughBoxId;
     }
